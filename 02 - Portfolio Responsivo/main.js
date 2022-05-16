a0a06e8519545b6bca0c819e847821a77776b9e1
djairmaykon <djairmaykon@gmail.com>
2022-05-16 07:48:12 -0300

    feat: envio de email por EmailJs e ajeita estilização de font dos inputs

--- 02 - Portfolio Responsivo/main.js
+++ 02 - Portfolio Responsivo/main.js
@@ -17,3 +17,29 @@ function closeMenu() {
   var checkBox = document.getElementById("toogle-menu-checkbox");
   checkBox.checked = false;
 }
+
+emailjs.init("9LoDMOhDM1BNQnZdU");
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function sendForm(event) {
+    event.preventDefault();
+
+    let name = event.target.name.value;
+    let email = event.target.email.value;
+    let message = event.target.message.value;
+
+    let contactParams = {
+      from_name: name,
+      from_email: email,
+      message: message,
+    };
+
+    emailjs.send("service_u8zyt0s", "template_odwgcig", contactParams).then(
+      function () {
+        alert("Email enviado com sucesso");
+      },
+      function (error) {
+        alert("Falha ao enviar o email por favor tente mais tarde");
+      }
+    );
+  });
