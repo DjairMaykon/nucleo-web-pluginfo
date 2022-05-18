6e3f3d82079acf117fd068ebdb963f33ca7c88fc
djairmaykon <djairmaykon@gmail.com>
2022-05-18 16:47:51 -0300

    wip: adiciona carrosel de filmes

--- 03 - MovieDB/themovinfo/src/App.tsx
+++ 03 - MovieDB/themovinfo/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Header } from "./components/header";
 import { MovieCard } from "./components/movie_card";
@@ -7,6 +7,7 @@ import { Movie } from "./util/types";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     api
@@ -36,22 +37,59 @@ function App() {
       });
   }, []);
 
+  function scrollCarousel(left: number) {
+    if (carouselRef.current) {
+      const atualLeft = carouselRef.current.scrollLeft;
+      carouselRef.current.scrollTo({ left: atualLeft + left });
+    }
+  }
+
   return (
     <>
       <Header />
-      <div className="w-fit mx-auto my-5 flex flex-wrap gap-3 justify-center">
-        {movies.length > 0
-          ? movies.map((movie, index) => {
-              return <MovieCard key={index} movie={movie} />;
-            })
-          : [...Array(20)].map((e, i) => {
-              return (
-                <div
-                  key={i}
-                  className="rounded-md animate-pulse bg-gray-300 flex w-[225px] h-[337.5px]"
-                ></div>
-              );
-            })}
+      <div className=" my-5 flex relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="relative w-12 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+          onClick={() => scrollCarousel(-225)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+        <div className="flex gap-3 overflow-scroll w-full" ref={carouselRef}>
+          <div className="flex gap-3 relative">
+            {movies.length > 0
+              ? movies.map((movie, index) => {
+                  return <MovieCard key={index} movie={movie} />;
+                })
+              : [...Array(20)].map((e, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className="rounded-md animate-pulse bg-gray-300 flex w-[225px] h-[337.5px]"
+                    ></div>
+                  );
+                })}
+          </div>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="relative w-12 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+          onClick={() => scrollCarousel(225)}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
       </div>
     </>
   );
