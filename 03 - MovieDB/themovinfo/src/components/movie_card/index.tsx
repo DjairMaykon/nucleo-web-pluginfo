a985111c7df1ab13400319cb75075cb9e24d3e1b
djairmaykon <djairmaykon@gmail.com>
2022-05-17 21:13:37 -0300

    style: aumenta tamanho texto de quantidade de votos

--- 03 - MovieDB/themovinfo/src/components/movie_card/index.tsx
+++ 03 - MovieDB/themovinfo/src/components/movie_card/index.tsx
@@ -25,7 +25,7 @@ export function MovieCard({ movie }: MovieCardProps) {
               >
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
               </svg>
-              <span className="absolute text-[8px] top-0 right-0 inline-flex items-center justify-center px-1 py-0.5 font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+              <span className="absolute text-[0.6rem] top-0 right-0 inline-flex items-center justify-center px-1 py-0.5 font-bold text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
                 {(movie?.voteAverage ?? 0) * 10}%
               </span>
             </span>
